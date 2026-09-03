73bf75aea173899682e59800f4b64f388982024f
chadP-cyb <cedreipacia@gmail.com>
2026-09-04 01:43:38 +0800

    deebuug

--- Jagged arrays/Program.cs
+++ Jagged arrays/Program.cs
@@ -4,22 +4,50 @@
 namespace DSA_ACT_3
 {
     class Program
-{
+    {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Code: 1 - Car, 2 - Bus, 3 - Subway, 4 - Bike, 5 - On foot");
             int[][] intJagged = new int[6][];
-            string[][] months = { "January", "February", "March", "April", "May", "June" };
+            string[] months = { "January", "February", "March", "April", "May", "June" };
 
-                for (int i = 0; i < intJagged.Length; i++)
+            for (int i = 0; i < intJagged.Length; i++)
             {
-                Console.WriteLine("Month of " + months[i] + ":");
+                Console.Write($"Enter number of entries for {months[i]}: ");
+                int count = Convert.ToInt32(Console.ReadLine());
+
+                intJagged[i] = new int[count];
+
+
+                Console.WriteLine($"Month of {months[i]}:");
 
                 for (int j = 0; j < intJagged[i].Length; j++)
                 {
 
-                    Console.WriteLine("  Enter Transportation Code:  ");
-                    intJagged[i][j]
+                    Console.Write("  Enter Transportation Code:  ");
+                    intJagged[i][j] = Convert.ToInt32(Console.ReadLine());
+                }
+
+
+                Console.WriteLine();
+            }
+        Console.Clear();
+
+            for (int i = 0; i < intJagged.Length; i++) 
+            {
+                Console.Write($"{months[i]}\t");
+
+                for (int j = 0; j < intJagged[i].Length; j++)
+                {
+
+                    Console.Write($"{intJagged[i][j]} ");
+                }
+                Console.WriteLine();
+
+            }
+            Console.ReadKey();
+
+
            
                
         }
