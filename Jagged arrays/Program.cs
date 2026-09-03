a90204144662dd5d286689c16f3416dba26b01b6
cedre <cedre@DRE>
2026-09-04 00:40:52 +0800

    Add project files.

---
+++ Jagged arrays/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace DSA_ACT_3
+{
+    class Program
+{
+        static void Main(string[] args)
+        {
+
+            int[][] intJagged = new int[6][];
+            string[][] months = { "January", "February", "March", "April", "May", "June" };
+
+                for (int i = 0; i < intJagged.Length; i++)
+            {
+                Console.WriteLine("Month of " + months[i] + ":");
+
+                for (int j = 0; j < intJagged[i].Length; j++)
+                {
+
+                    Console.WriteLine("  Enter Transportation Code:  ");
+                    intJagged[i][j]
+           
+               
+        }
+}
+
+    }
